26c3635a2f71205d3e81235c4848a2257784d357
asanka <asanka.abewickrama@gmail.com>
2024-10-19 23:19:41 +0530

    updates

--- src/app/(app)/about/page.jsx
+++ src/app/(app)/about/page.jsx
@@ -2,27 +2,20 @@ import { getUser } from "@/hooks/useData"
 import * as motion from "framer-motion/client"
 import { parent } from "../animation";
 import { PortableText } from "next-sanity";
-<<<<<<< HEAD
 import Spacer from "@/components/Spacer";
-=======
-import { components } from "@/components/PortableText";
->>>>>>> main
+import { description } from "@/components/PortableComponents";
 
 export default async function Page() {
 
     const user = await getUser();
+    
     return (
         <motion.div  variants={parent} viewport={{once: true}} initial="hidden" whileInView="visible" className="container w-full mx-auto px-3">
             <Spacer/>
             <section className="grid grid-cols-12">
                 <div className="grid gap-2 col-span-12 md:col-span-8">
-<<<<<<< HEAD
-                    <h2 className="text-2xl md:text-2xl">About</h2>
-                    <PortableText value={user.description}/>
-=======
                     <h2 className="text-2xl md:text-2xl mb-2">About</h2>
-                    <PortableText value={user.description} components={components} />
->>>>>>> main
+                    <PortableText value={user.description} components={description} />
                 </div>
             </section>
         </motion.div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,9 +12,9 @@ export default function Card({href, title, date, image}) {
             <div className="overflow-hidden flex justify-center items-center rounded-lg border border-black/5 aspect-video">
                 <Image src={urlFor(image).width(900).url()} width={900} height={900} alt="card image" className="transition group-hover:scale-110" />
             </div>
-            <div className="flex justify-between pt-2">
-                <h1 className="font-medium transition origin-left opacity-70 group-hover:opacity-100">{title}</h1>
-                <p className="text-xs transition duration-700 origin-left opacity-70">{date}</p>
+            <div className="flex justify-between pt-3">
+                <h1 className="font-medium transition opacity-70 group-hover:opacity-100">{title}</h1>
+                <p className="text-xs opacity-70">{date}</p>
             </div>
         </Link>
         </motion.div>
