5972a2ae7bd2a5e5650221702cb23f663e25e0d6
asankaab <asanka.abewickrama@gmail.com>
2025-04-28 19:43:20 +0530

    Refactor Sanity CLI configuration: remove environment variables and set studioHost to 'asanka'

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,17 +2,14 @@
 * This configuration file lets you run `$ sanity [command]` in this folder
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
+import { dataset, projectId } from '@/sanity/env';
 import { defineCliConfig } from 'sanity/cli'
 // import { dataset, projectId } from './src/sanity/env'
-
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const hostName = process.env.SANITY_STUDIO_HOSTNAME
 
 export default defineCliConfig({
   api: {
     projectId: projectId,
     dataset: dataset
   },
-  studioHost: hostName,
+  studioHost: 'asanka',
 });
