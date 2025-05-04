1db974473ca2ba9119b887f3a346a926f88bba14
asankaab <asanka.abewickrama@gmail.com>
2025-05-04 13:18:50 +0530

    feat: update CLI configuration to use environment variables for projectId, dataset, and studioHost

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,16 +2,16 @@
 * This configuration file lets you run `$ sanity [command]` in this folder
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
-import { dataset, projectId } from '@/sanity/env';
+import { dataset, projectId, studioHost } from '@/sanity/env';
 import { defineCliConfig } from 'sanity/cli'
 // import { dataset, projectId } from './src/sanity/env'
 
 export default defineCliConfig({
   api: {
-    projectId: 'm74nhq4q',
-    dataset: 'production'
+    projectId: projectId,
+    dataset: dataset
   },
-  studioHost: 'asanka',
+  studioHost: studioHost,
   project: {
     basePath: '/'
   }
--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,3 +1,4 @@
 export const apiVersion = "2025-02-10"
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET;
+export const studioHost = process.env.SANITY_STUDIO_HOST || process.env.SANITY_STUDIO_HOST;
