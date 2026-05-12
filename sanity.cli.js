68a6c9fd45720cdef54714c5b42dd110529fb93d
asankaab <asanka.abewickrama@gmail.com>
2026-05-12 20:59:39 +0530

    sanity, next-sanity, nextjs upgraded

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,7 +2,7 @@
 * This configuration file lets you run `$ sanity [command]` in this folder
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
-import { dataset, projectId, studioHost } from '@/sanity/env';
+import { dataset, projectId, studioHost } from './src/sanity/env';
 import { defineCliConfig } from 'sanity/cli'
 // import { dataset, projectId } from './src/sanity/env'
 
@@ -14,5 +14,8 @@ export default defineCliConfig({
   studioHost: studioHost,
   project: {
     basePath: '/'
+  },
+  deployment: {
+    appId: 'nbttepor4nt2g0ax6pwa9lj4',
   }
 });
